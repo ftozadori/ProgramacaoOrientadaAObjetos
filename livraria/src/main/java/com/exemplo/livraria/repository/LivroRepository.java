0f544c47cf4ad88aa4bdd86f4e2b05fc08e5fa94
Felipe Tozadori Silva <ftozadori@gmail.com>
2026-02-25 15:07:58 -0300

    Delete livraria directory

--- livraria/src/main/java/com/exemplo/livraria/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.livraria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.exemplo.livraria.modal.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
